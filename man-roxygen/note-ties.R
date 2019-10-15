17680ea3a75952db29dd8bb3c6c761ad1be63936
Steven Pav <shabbychef@gmail.com>
2019-10-14 21:32:19 -0700

    note on ties.

--- man-roxygen/note-ties.R
+++ man-roxygen/note-ties.R
@@ -2,3 +2,7 @@
 #' This regression may give odd results when the outcomes are tied,
 #' imposing an arbitrary order on the tied outcomes.
 #' Moreover, no warning may be issued in this case.
+#' In future releases, ties may be dealt with differently,
+#' perhaps in analogy to how ties are treated in the
+#' Cox Proportional Hazards regression, using
+#' the methods of Breslow or Efron.
